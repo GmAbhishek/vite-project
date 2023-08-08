fc63183a6e7b43dd29704a6fdbc443659865fdcd
Gm_Abhishek <gmabhishek2004@gmail.com>
2023-08-08 08:32:58 +0530

    Added key to list element

--- src/hello.tsx
+++ src/hello.tsx
@@ -2,9 +2,10 @@ function HelloWorld() {
   const ListItems = ["hello", "world", " hi", "nigga"];
   return (
     <>
+    <h1>List Group</h1>
       <ul className="list-group">
         {ListItems.map((item) => (
-          <li>{item}</li>
+          <li key={item}>{item}</li>
         ))}
       </ul>
     </>
